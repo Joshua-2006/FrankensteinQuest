fe728f2942b216fd809f996952b83f3e0d13ee80
Joshua Jopling <149619818+Joshua-2006@users.noreply.github.com>
2025-02-26 08:57:31 -0800

    LKJF:LKSJFS:LKdhaj;lkfghd

---
+++ Assets/Josh/Scripts/Door.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Box"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Josh/Scripts/PlayerContrlr.cs
+++ Assets/Josh/Scripts/PlayerContrlr.cs
@@ -6,16 +6,34 @@ public class PlayerContrlr : MonoBehaviour
 {
     public float horizontal;
     public float vertical;
+    public float speed;
+    public Rigidbody2D rb;
+    public bool grounded;
+    public float jumpForce;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector2.right * speed * horizontal, ForceMode2D.Impulse);
         
+        if(Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            grounded = false;
+            rb.AddForce(Vector2.up * jumpForce * vertical, ForceMode2D.Impulse);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
     }
 }
