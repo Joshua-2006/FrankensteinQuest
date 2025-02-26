ba5b20ede538912963e395636065c6e8dda865ac
Joshua Jopling <149619818+Joshua-2006@users.noreply.github.com>
2025-02-26 09:58:37 -0800

    Player Controller Works

--- Assets/Josh/Scripts/Door.cs
+++ Assets/Josh/Scripts/Door.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    public GameObject uiElement;
+    public string nextLevel;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Box"))
+        if(collision.CompareTag("Player"))
         {
-            Destroy(gameObject);
-            Destroy(collision.gameObject);
+            uiElement.SetActive(true);
+            if(Input.GetButtonDown("Interact"))
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        uiElement.SetActive(false);
     }
 }
--- Assets/Josh/Scripts/PlayerContrlr.cs
+++ Assets/Josh/Scripts/PlayerContrlr.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerContrlr : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class PlayerContrlr : MonoBehaviour
     public Rigidbody2D rb;
     public bool grounded;
     public float jumpForce;
+    public int jumps = 5;
     
     // Start is called before the first frame update
     void Start()
@@ -20,20 +22,35 @@ public class PlayerContrlr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        if(Input.GetKeyDown(KeyCode.Space) && jumps > 0)
+        {
+            jumps -= 1;
+            grounded = false;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if(grounded == false)
+        {
+            rb.gravityScale = 4; 
+        }
+    }
+    private void FixedUpdate()
+    {
         horizontal = Input.GetAxis("Horizontal");
         rb.AddForce(Vector2.right * speed * horizontal, ForceMode2D.Impulse);
-        
-        if(Input.GetKeyDown(KeyCode.W) && grounded)
-        {
-            grounded = false;
-            rb.AddForce(Vector2.up * jumpForce * vertical, ForceMode2D.Impulse);
-        }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
             grounded = true;
+            rb.gravityScale = 1;
+            jumps = 1;
+        }
+        if(collision.gameObject.CompareTag("Reset"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
