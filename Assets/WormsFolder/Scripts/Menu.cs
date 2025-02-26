d092b8c09f4ea5f0b369da99f1c9f7d3080735a7
ISAIAH BRITT <wormgamedevelopment@gmail.com>
2025-02-26 09:39:41 -0800

    Added controller support for main menu and added other 2 scripts

--- Assets/WormsFolder/Scripts/Menu.cs
+++ Assets/WormsFolder/Scripts/Menu.cs
@@ -5,16 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+   
     public void OnPlayButton(string nextLevel)
     {
         SceneManager.LoadScene(nextLevel);
+
+       
     }
     public void OnMenuButton(string nextLevel)
     {
         SceneManager.LoadScene(nextLevel);
+
+       
     }
     public void OnApplicationQuit()
     {
         Application.Quit();
+
+       
     }
 }
